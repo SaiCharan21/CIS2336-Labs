426be18bc2d3704d42d269eb0fed873b90c495b2
Sai Charan Todupunoori <tsaicharan03@gmail.com>
2022-09-27 20:04:39 -0500

    First Commit

---
+++ Week6/js/Week06.js
@@ -0,0 +1,6 @@
+document.write('This is external JS')
+document.write('<br>'+'Hello, welcome to CIS 2336')
+document.write('<br>'+'My name is Sai')
+document.write('<br>'+'I am in the second year of college')
+
+
